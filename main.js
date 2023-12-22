0373f8f21a7b461c0607a4f59cf39c8af41932df
Mevt17 <mevt07@gmail.com>
2023-12-22 17:03:10 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// let persona= {
+//     nombre: "Ale",
+//      apellido: "Campos"
+// }
+
+// class Persona {
+//     constructor(nombre, apellido){
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//     }
+//     saludar(){
+//         console.log("Hola, mi nombre es" + this.nombre)
+//     }
+
+// }
+
+// let persona2 = new Persona("jose", "Perez")
+
+
+//  persona2.saludar();
+
+// persona2.apellido="gomez";
+// console.log (persona2.apellido) 
+// let nombre = prompt("Ingrese su nombre");
+let titulo = document.getElementById("titulo");
+
+// titulo.innerText = "Bienvenido" + nombre;
+
+console.log(titulo.classList.value)
+
+titulo.classList.value= "otraClase"
+let contador = 0;
+
+function saludar(){
+    // contador++;
+    // console.log("hola cara de bola")
+   let input= document.getElementById("ingreso");
+    console.log(input)
+}
