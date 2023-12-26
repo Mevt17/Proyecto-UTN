0b0c4273d0b8db56732863078adb675b0d894242
Mevt17 <mevt07@gmail.com>
2023-12-25 21:09:03 -0300

    Modifcaciones estructura + boceto formulario

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-let 
+let currentStep = 1;
+
+function showProcess() {
+    let proceso = document.getElementById('process-container')
+    proceso.style.display = "block";
+}
+
+function nextStep(step){
+    
+}
+
+
+
