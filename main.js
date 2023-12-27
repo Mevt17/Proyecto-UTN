a383211d94b2b5fac818e6cfd5f8e41ac8ebc823
Mevt17 <mevt07@gmail.com>
2023-12-26 23:01:32 -0300

    Cambio de estructura y diseño, agrego animación

--- main.js
+++ main.js
@@ -2,12 +2,37 @@ let currentStep = 1;
 
 function showProcess() {
     let proceso = document.getElementById('process-container')
-    proceso.style.display = "block";
+    proceso.style.display = 'block';
+
+    showProcess(currentStep);
+}
+function showStep(step){
+    let paso = document.getElementById('step'+ step);
+    paso.style.display='block'
+}
+
+function hideStep(step){
+    document.getElementById('step'+step).style.display= 'none';
 }
 
 function nextStep(step){
+    hideStep(currentStep);
+    currentStep = step + 1;
+    showStep(currentStep);
+}
+
+function prevStep(step){
+    hideStep(currentStep);
+    currentStep = step - 1;
+    showStep(currentStep);
+
+}
+
+function send(event){
+    alert('Bienvenido a la comunidad')
+    document.getElementById('process-container')
     
 }
 
-
+showStep(currentStep);
 
