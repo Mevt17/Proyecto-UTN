edffbf70238e96f2b7320905938f7f5ac46d3807
Mevt17 <mevt07@gmail.com>
2023-12-27 21:20:01 -0300

    Modificación estética+enlaces+js

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 let currentStep = 1;
+let nombre;
+let apellido;
+let email;
+
 
 function showProcess() {
     let proceso = document.getElementById('process-container')
-    proceso.style.display = 'block';
-
-    showProcess(currentStep);
+    proceso.style.display = "block";
+    
 }
-function showStep(step){
-    let paso = document.getElementById('step'+ step);
-    paso.style.display='block'
+
+function showStep(step) {
+    let paso = document.getElementById('step'+step);
+    paso.style.display = 'block';
 }
 
 function hideStep(step){
-    document.getElementById('step'+step).style.display= 'none';
+    document.getElementById('step'+step).style.display = 'none';
 }
 
 function nextStep(step){
@@ -29,10 +33,23 @@ function prevStep(step){
 }
 
 function send(event){
-    alert('Bienvenido a la comunidad')
-    document.getElementById('process-container')
+
+    event.preventDefault();
+
+    nombre = document.getElementById('nombre').value;
+    apellido = document.getElementById('Apellido').value;
+    email = document.getElementById('Email').value;
+    alert('Bienvenido a la comunidad');
+    console.log(nombre, apellido, email);
+    // document.getElementById('process-container').style.display = 'none';
     
+
+    let datos = document.getElementById('user-data');
+    datos.innerHTML = `
+    <p>Nombre:${nombre}</p>
+    <p>Apellido:${apellido}</p>
+    <p>Email:${email}</p>
+    `
 }
 
 showStep(currentStep);
-
